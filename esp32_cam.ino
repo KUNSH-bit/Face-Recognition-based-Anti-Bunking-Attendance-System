b2771bacd5bdccd1092ebc59b935b67051b4adec
KUNSH-bit <agarwalkunsh54@gmail.com>
2026-06-13 15:02:48 +0530

    Create esp32_cam.ino

---
+++ esp32_cam.ino
@@ -0,0 +1,12 @@
+/*
+ Face Recognition Attendance System
+
+ ESP32-CAM acts as a video streaming device.
+ Python/OpenCV performs face recognition
+ and attendance logging.
+
+ Configure Wi-Fi credentials below.
+*/
+
+const char* ssid = "YOUR_WIFI";
+const char* password = "YOUR_PASSWORD";
